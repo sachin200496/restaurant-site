c4ebdb264f9ca7f941e43b1d1627aa84c01dcd80
Sachin Rajbhar <sachin200496@gmail.com>
2023-04-02 18:50:46 +0000

    Update 3 files

---
+++ src/components/hero.js
@@ -0,0 +1,10 @@
+import './hero.css';
+
+export default function Hero(){
+    
+    return(
+        <div className='hero'>
+          <img className='hero-bg' src={require('')}>
+        </div>
+    )
+}
