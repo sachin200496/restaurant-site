e6d2a63e0b5f835b1df60137f81d2add6890b298
Sachin Rajbhar <sachin200496@gmail.com>
2023-04-03 18:20:39 +0000

    Update 4 files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Navbar from "./components/navbar";
 import Form from "./components/form";
+import Hero from './components/hero';
 import "./styles.css";
 
 export default function App() {
   return (
     <div className="App">
       <Navbar />
+      <Hero/>
     </div>
   );
 }
--- src/components/hero.js
+++ src/components/hero.js
@@ -1,10 +1,14 @@
-import './hero.css';
+import "./hero.css";
 
-export default function Hero(){
-    
-    return(
-        <div className='hero'>
-          <img className='hero-bg' src={require('')}>
-        </div>
-    )
+export default function Hero() {
+  return (
+    <div className="hero">
+      <img
+        className="hero-bg"
+        src={require("../images/hero-food-bg.jpg")}
+        alt="food-hero"
+      />
+      <h2>Bon Appétit</h2>
+    </div>
+  );
 }
